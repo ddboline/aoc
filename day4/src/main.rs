23e94da9d3352829a790a67974478407efaf94bb
Daniel Boline <ddboline@gmail.com>
2022-12-05 17:59:32 -0500

    use ranges instead of hashsets

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,10 +1,9 @@
-use clap::Parser;
 use anyhow::Error;
-use std::path::PathBuf;
+use clap::Parser;
 use smallvec::SmallVec;
-use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Input {
@@ -23,61 +22,92 @@ fn main() -> Result<(), Error> {
 
 fn simple_iterator(p: &Path) -> Result<u64, Error> {
     let buf = fs::read_to_string(p)?;
-    let overlaps = buf.split('\n').map(|s| {
-        if s.is_empty() {
-            0
-        } else {
-            check_if_ranges_fully_overlap(s) as u64
-        }
-    }).sum();
+    let overlaps = buf
+        .split('\n')
+        .map(|s| {
+            if s.is_empty() {
+                0
+            } else {
+                check_if_ranges_fully_overlap(s) as u64
+            }
+        })
+        .sum();
     Ok(overlaps)
-}
-
-fn convert_range_to_vec(s: &str) -> HashSet<u64> {
-    let v: SmallVec<[&str; 2]> = s.split('-').take(2).collect();
-    let start: u64 = v[0].parse().ok().unwrap_or(0);
-    let end: u64 = v[1].parse().ok().unwrap_or(0);
-    assert!(start <= end);
-    (start..=end).collect()
-}
-
-fn check_if_ranges_fully_overlap(s: &str) -> bool {
-    let v: SmallVec<[&str; 2]> = s.split(',').collect();
-    let e0 = convert_range_to_vec(&v[0]);
-    let e1 = convert_range_to_vec(&v[1]);
-    let u01 = e0.union(&e1).count();
-    u01 == e0.len() || u01 == e1.len()
-}
-
-fn check_if_ranges_overlap(s: &str) -> bool {
-    let v: SmallVec<[&str; 2]> = s.split(',').collect();
-    let e0 = convert_range_to_vec(&v[0]);
-    let e1 = convert_range_to_vec(&v[1]);
-    e0.intersection(&e1).count() > 0
 }
 
 fn simple_iterator2(p: &Path) -> Result<u64, Error> {
     let buf = fs::read_to_string(p)?;
-    let overlaps = buf.split('\n').map(|s| {
-        if s.is_empty() {
-            0
-        } else {
-            check_if_ranges_overlap(s) as u64
-        }
-    }).sum();
+    let overlaps = buf
+        .split('\n')
+        .map(|s| {
+            if s.is_empty() {
+                0
+            } else {
+                check_if_ranges_overlap(s) as u64
+            }
+        })
+        .sum();
     Ok(overlaps)
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum RangeOverlaps {
+    NoOverlap,
+    PartialOverlap,
+    FullOverlap,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Range {
+    start: u64,
+    end: u64,
+}
+
+fn convert_str_to_range(s: &str) -> Range {
+    let v: SmallVec<[&str; 2]> = s.split('-').take(2).collect();
+    let start: u64 = v[0].parse().ok().unwrap_or(0);
+    let end: u64 = v[1].parse().ok().unwrap_or(0);
+    assert!(start <= end);
+    Range { start, end }
+}
+
+fn get_range_overlap(r0: Range, r1: Range) -> RangeOverlaps {
+    let (lower, upper) = if r0.start <= r1.start {
+        (r0, r1)
+    } else {
+        (r1, r0)
+    };
+    if lower.end < upper.start {
+        RangeOverlaps::NoOverlap
+    } else if lower.start < upper.start && lower.end < upper.end {
+        RangeOverlaps::PartialOverlap
+    } else {
+        RangeOverlaps::FullOverlap
+    }
+}
+
+fn check_if_ranges_fully_overlap(s: &str) -> bool {
+    let v: SmallVec<[&str; 2]> = s.split(',').collect();
+    let r0 = convert_str_to_range(v[0]);
+    let r1 = convert_str_to_range(v[1]);
+    get_range_overlap(r0, r1) == RangeOverlaps::FullOverlap
+}
+
+fn check_if_ranges_overlap(s: &str) -> bool {
+    let v: SmallVec<[&str; 2]> = s.split(',').collect();
+    let r0 = convert_str_to_range(v[0]);
+    let r1 = convert_str_to_range(v[1]);
+    get_range_overlap(r0, r1) != RangeOverlaps::NoOverlap
 }
 
 #[cfg(test)]
 mod tests {
-    use maplit::hashset;
-
     use super::*;
 
     #[test]
     fn test_convert_range_to_vec() {
-        assert_eq!(convert_range_to_vec("2-4"), hashset![2,3,4]);
-        assert_eq!(convert_range_to_vec("6-6"), hashset![6]);
+        assert_eq!(convert_str_to_range("2-4"), Range { start: 2, end: 4 });
+        assert_eq!(convert_str_to_range("6-6"), Range { start: 6, end: 6 });
     }
 
     #[test]
