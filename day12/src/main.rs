7a453fc78809fbb58fbbc6b730b877e7c6b38418
Daniel Boline <ddboline@gmail.com>
2022-12-15 09:03:56 -0500

    refactor problem

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -16,9 +16,11 @@ fn main() -> Result<(), Error> {
     let mut map = Map::from_str(&buf).unwrap();
     let fewest_steps = map.fewest_number_of_steps().unwrap();
     println!("fewest steps {fewest_steps}");
+    assert_eq!(fewest_steps, 481);
     let mut map = Map::from_str(&buf).unwrap();
     let fewest_steps_any_a = map.fewest_steps_any_a().unwrap();
     println!("fewest_steps_any_a {fewest_steps_any_a}");
+    assert_eq!(fewest_steps_any_a, 480);
     Ok(())
 }
 
@@ -67,27 +69,12 @@ struct Map {
     width: usize,
     height: usize,
     start: (usize, usize),
+    end: (usize, usize),
     tentative: HashSet<(usize, usize)>,
     visited: HashSet<(usize, usize)>,
 }
 
 impl Map {
-    fn set_new_start(&mut self, start: (usize, usize)) -> Option<()> {
-        if start.0 > self.height || start.1 > self.width {
-            println!("start {start:?}");
-            return None;
-        }
-        for line in self.map.iter_mut() {
-            for node in line.iter_mut() {
-                node.min_steps = None;
-            }
-        }
-        self.start = start;
-        self.tentative.clear();
-        self.visited.clear();
-        Some(())
-    }
-
     fn from_str(buf: &str) -> Option<Self> {
         let map = buf.split('\n').fold(Vec::new(), |mut acc, line| {
             let map_line: Vec<_> = line.chars().filter_map(Node::from_char).collect();
@@ -97,29 +84,55 @@ impl Map {
             acc
         });
         let height = map.len();
-        if height == 0 {
-            return None;
-        }
-        let width = map[0].len();
+        let mut width = None;
         let mut start = None;
+        let mut end = None;
         for (idx, x) in map.iter().enumerate() {
-            if x.len() != width {
+            if width.is_none() {
+                width.replace(x.len());
+            }
+            if Some(x.len()) != width {
                 return None;
             }
             for (idy, y) in x.iter().enumerate() {
                 if y.height_char == 'S' {
                     start.replace((idx, idy));
+                } else if y.height_char == 'E' {
+                    end.replace((idx, idy));
                 }
             }
         }
         let start = start?;
-        Some(Self {
+        let end = end?;
+        let width = width?;
+        let final_map = Self {
             map,
             width,
             height,
             start,
+            end,
             ..Self::default()
-        })
+        };
+        if final_map.is_valid() {
+            Some(final_map)
+        } else {
+            None
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        if self.height == 0 {
+            return false;
+        }
+        if self.height != self.map.len() {
+            return false;
+        }
+        for x in self.map.iter() {
+            if x.len() != self.width {
+                return false;
+            }
+        }
+        true
     }
 
     fn get_a_positions(&self) -> HashSet<(usize, usize)> {
@@ -134,9 +147,9 @@ impl Map {
         a_positions
     }
 
-    fn get_next_node(&self) -> Option<(usize, usize)> {
+    fn get_next_node(&self, start: (usize, usize)) -> Option<(usize, usize)> {
         if self.tentative.is_empty() {
-            Some(self.start)
+            Some(start)
         } else {
             let mut min_step = None;
             let mut min_position = None;
@@ -161,7 +174,7 @@ impl Map {
     }
 
     fn fewest_number_of_steps(&mut self) -> Option<usize> {
-        while let Some((x, y)) = self.get_next_node() {
+        while let Some((x, y)) = self.get_next_node(self.start) {
             let current_steps = self.map.get(x)?.get(y)?.min_steps.unwrap_or(0);
             self.map
                 .get_mut(x)?
@@ -211,24 +224,66 @@ impl Map {
     }
 
     fn fewest_steps_any_a(&mut self) -> Option<usize> {
-        let mut min_steps = None;
-        let a_positions = self.get_a_positions();
-        println!("a_positions {}", a_positions.len());
+        let mut min_steps: Option<usize> = None;
+        let all_a_nodes = self.get_a_positions();
+        let mut visited_a_positions = HashSet::new();
 
-        for (n, (x, y)) in a_positions.into_iter().enumerate() {
-            self.set_new_start((x, y))?;
-            if let Some(s) = self.fewest_number_of_steps() {
-                if n % 100 == 0 {
-                    println!("{n} number of steps {s}");
+        while let Some((x, y)) = self.get_next_node(self.end) {
+            let current_steps = self.map.get(x)?.get(y)?.min_steps.unwrap_or(0);
+            self.map
+                .get_mut(x)?
+                .get_mut(y)?
+                .min_steps
+                .replace(current_steps);
+            let current_steps = current_steps + 1;
+
+            let current_node = *self.map.get(x)?.get(y)?;
+            if let Some(up_node) = self.map.get_mut(x + 1).and_then(|v| v.get_mut(y)) {
+                if up_node.is_valid_next_step(current_node) {
+                    up_node.increment_node(current_steps);
+                    self.tentative.insert((x + 1, y));
                 }
-                if let Some(ms) = min_steps {
-                    if s < ms {
-                        println!("{n} number of steps {s}");
-                        min_steps.replace(s);
+            }
+            if let Some(right_node) = self.map.get_mut(x).and_then(|v| v.get_mut(y + 1)) {
+                if right_node.is_valid_next_step(current_node) {
+                    right_node.increment_node(current_steps);
+                    self.tentative.insert((x, y + 1));
+                }
+            }
+            if x > 0 {
+                if let Some(down_node) = self.map.get_mut(x - 1).and_then(|v| v.get_mut(y)) {
+                    if down_node.is_valid_next_step(current_node) {
+                        down_node.increment_node(current_steps);
+                        self.tentative.insert((x - 1, y));
+                    }
+                }
+            }
+            if y > 0 {
+                if let Some(left_node) = self.map.get_mut(x).and_then(|v| v.get_mut(y - 1)) {
+                    if left_node.is_valid_next_step(current_node) {
+                        left_node.increment_node(current_steps);
+                        self.tentative.insert((x, y - 1));
+                    }
+                }
+            }
+            self.visited.insert((x, y));
+            if self.tentative.contains(&(x, y)) {
+                self.tentative.remove(&(x, y));
+            }
+            if current_node.height_char == 'a' {
+                visited_a_positions.insert((x, y));
+
+                if let Some(m) = min_steps {
+                    let current_steps = current_node.min_steps?;
+                    if current_steps < m {
+                        min_steps.replace(current_steps);
                     }
                 } else {
-                    println!("{n} number of steps {s}");
-                    min_steps.replace(s);
+                    min_steps = current_node.min_steps;
+                }
+
+                if visited_a_positions.len() == all_a_nodes.len() {
+                    return min_steps;
                 }
             }
         }
